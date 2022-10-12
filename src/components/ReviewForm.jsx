af84aafef5631b6ebe7b7f91c1c48863421e1095
ren831 <rene83120@gmail.com>
2022-10-11 20:15:05 -0700

    update

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,3 +1,12 @@
-function ReviewForm(params) {
-  return;
+function ReviewForm() {
+  return (
+    <div>
+      <form>
+        <textarea name="review" cols="40" rows="10"></textarea>
+        <button type="submit">Add Review</button>
+      </form>
+    </div>
+  );
 }
+
+export default ReviewForm;
