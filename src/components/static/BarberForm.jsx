3f4dc503209fc67fafe7b45572ccc633471e0b37
ren831 <rene83120@gmail.com>
2022-10-11 19:12:25 -0700

    update

---
+++ src/components/static/BarberForm.jsx
@@ -0,0 +1,13 @@
+function BarberForm() {
+  return (
+    <div>
+      <h3>Add New Barber</h3>
+      <form>
+        <input type="text" name="Name" placeholder="Name" />
+        <button type="submit">Add Barber</button>
+      </form>
+    </div>
+  );
+}
+
+export default BarberForm;
