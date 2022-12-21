0f118a0d1ffdcf9f2e578c6cacefa5aff068a601
ren831 <rene83120@gmail.com>
2022-12-20 19:03:17 -0800

    updated

--- src/components/Barbers.jsx
+++ src/components/Barbers.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReviewForm from "./ReviewForm";
 import BarberForm from "./static/BarberForm";
-import Navbar from "./static/Navigation/Navbar";
 
 function Barbers() {
   return (
