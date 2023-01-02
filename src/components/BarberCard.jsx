f2b412264bade50b68a6b5dd5acfa0130c726c13
ren831 <rene83120@gmail.com>
2023-01-01 20:02:12 -0800

    updated

--- src/components/BarberCard.jsx
+++ src/components/BarberCard.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 function BarberCard({ barber }) {
   console.log(barber);
-  return;
-
-  <div class="ui cards">
-    <div class="card">
-      <div class="content">
-        <div class="header">Elliot Fu</div>
-        <div class="meta">Friend</div>
-        <div class="description">Elliot Fu is a film-maker from New York.</div>
-      </div>
-    </div>
-  </div>;
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>
+          <b>{barber.name}</b>
+        </Card.Header>
+        id:{barber.id}
+      </Card.Content>
+      <Card.Meta>{}</Card.Meta>
+    </Card>
+  );
 }
 
 export default BarberCard;
